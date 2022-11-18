06e7d4d6491e26a7b56289bbf9ca1f12548375ef
Israel Kollie <israelkollie@Israels-MacBook-Pro.local>
2022-11-18 11:34:06 +0000

    update deployment scripts

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,25 @@
-from src import app
+import os
+import flask
+
+app = flask.Flask(__name__)
+app.debug = True
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+def hello_world():
+  storage = Storage()
+  storage.populate()
+  score = storage.score()
+  return "Hello world, %d!" % score
+
+class Storage():
+  def __init__(self):
+    self.db = dict()
+
+  def populate(self):
+    self.db['score'] = 1234
+
+  def score(self):
+    return self.db['score']
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0')
