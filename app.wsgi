7f6ff315293815f73aafb2bcbc87337dd0526d6b
Israel Kollie <israelkollie@Israels-MacBook-Pro.local>
2022-11-21 13:15:11 +0000

    updata app for beanstalk

--- app.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/home/ec2-user/server3')
-
-from helloworld import application
--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug = False)
-
-from src import routes
