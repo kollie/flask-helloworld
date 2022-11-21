cd45ae50c7f133b34f05001c8f246a9d1b319946
Israel Kollie <israelkollie@Israels-MacBook-Pro.local>
2022-11-21 13:37:11 +0000

    update application

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = flask.Flask(__name__)
 
 @application.route('/')
 def hello_world():
-  return "Testing flask app..."
+  return "Testing app github pipeling.."
 
 if __name__ == "__main__":
   application.run()
